ece3459828508180cbdfe3b38583e77c822ef968
Jim Holmberg <hasenpfad1@gmail.com>
2019-06-14 12:58:23 -0500

    Add files via upload

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,7 +36,7 @@ function showAll (){
 }
 
 function runSearch(res) {
-  
+  connection.query("SELECT * FROM products", function(err, res) {
   var choicesArray = [];
   for (var i = 0; i < res.length; i++) {
     //console.log("Product name is " + res[i].product_name);
@@ -58,61 +58,8 @@ function runSearch(res) {
     }]).then(function(answer){
       console.log("The chosen item is " + answer.itemName);
       console.log("The number of chosen item is " + answer.quantity);
-
+      
     })
-    //console.log("The chosen item is " + answer.itemName);
-  /*  .then(function(answer) {
-      // get the information of the chosen item
-      var chosenItem;
-      for (var i = 0; i < results.length; i++) {
-        if (results[i].item_name === answer.choice) {
-          chosenItem = results[i];
-        }
-      }
+  });
 
-      // determine if bid was high enough
-      if (chosenItem.highest_bid < parseInt(answer.bid)) {
-        // bid was high enough, so update db, let the user know, and start over
-        connection.query(
-          "UPDATE auctions SET ? WHERE ?",
-          [
-            {
-              highest_bid: answer.bid
-            },
-            {
-              id: chosenItem.id
-            }
-          ],
-          function(error) {
-            if (error) throw err;
-            console.log("Bid placed successfully!");
-            start();
-          }
-        );
-      }
-      else {
-        // bid wasn't high enough, so apologize and start over
-        console.log("Your bid was too low. Try again...");
-        start();
-      }
-    });
-    */
 }
-/*function processOrder(item) {
-  inquirer
-    .prompt(
-      {
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to buy? ",
-      //ASK TA'S HOW TO SET UP A LOOP
-      choices: choicesArray
-    }
-    )
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      processOrder(answer);
-    });
-    */
-
-
